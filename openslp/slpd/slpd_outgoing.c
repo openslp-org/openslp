1320571da2a4075456514292e674ed8d937a1035
mpeterson <mpeterson@51fdc78b-28a9-475c-aff9-d797dd46749a>
2000-08-17 19:18:24 +0000

    Handles "outgoing" network conversations requests made by slpd to other agents

---
+++ openslp/slpd/slpd_outgoing.c
@@ -0,0 +1,59 @@
+/***************************************************************************/
+/*                                                                         */
+/* Project:     OpenSLP - OpenSource implementation of Service Location    */
+/*              Protocol Version 2                                         */
+/*                                                                         */
+/* File:        slpd_outgoing.c                                            */
+/*                                                                         */
+/* Abstract:    Handles "outgoing" network conversations requests made by  */
+/*              slpd to agents to slpd. (slpd_incoming.c handles reqests   */
+/*              made by other agents to slpd)                               */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/* Copyright (c) 1995, 1999  Caldera Systems, Inc.                         */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU Lesser General Public License as published   */
+/* by the Free Software Foundation; either version 2.1 of the License, or  */
+/* (at your option) any later version.                                     */
+/*                                                                         */
+/*     This program is distributed in the hope that it will be useful,     */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of      */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       */
+/*     GNU Lesser General Public License for more details.                 */
+/*                                                                         */
+/*     You should have received a copy of the GNU Lesser General Public    */
+/*     License along with this program; see the file COPYING.  If not,     */
+/*     please obtain a copy from http://www.gnu.org/copyleft/lesser.html   */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/*     Please submit patches to http://www.openslp.org                     */
+/*                                                                         */
+/***************************************************************************/
+
+#include "slpd.h"
+
+
+/*=========================================================================*/
+void SLPDOutgoingHandler(SLPDSocketList* list, 
+                         int* fdcount,
+                         fd_set* readfds,
+                         fd_set* writefds)
+
+/* Handles all incoming requests that are pending on the specified file    */
+/* discriptors                                                             */
+/*                                                                         */
+/* list     (IN) list of incoming sockets                                  */
+/*                                                                         */
+/* fdcount  (IN/OUT) number of file descriptors marked in fd_sets          */
+/*                                                                         */
+/* readfds  (IN) file descriptors with pending read IO                     */
+/*                                                                         */
+/* writefds  (IN) file descriptors with pending read IO                    */
+/*=========================================================================*/
+
+{
+    /* TODO: put something here! */
+}
