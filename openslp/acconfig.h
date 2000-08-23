ebbf2eb8f0326f25e9e4eb10e815c9db89b3ab9e
(no author) <(no author)@51fdc78b-28a9-475c-aff9-d797dd46749a>
2000-08-23 18:28:18 +0000

    This commit was manufactured by cvs2svn to create tag 'OpenSLP_0-7-2'.

--- openslp/acconfig.h
+++
@@ -1,7 +0,0 @@
-/* Local acconfig.h */
-
-/* defined if struct sigaction has member sa_restorer */
-#define HAVE_SA_RESTORER
-
-/* defined to size_t if <sys/socket.h> does not support socklen_t data type */
-#define socklen_t
