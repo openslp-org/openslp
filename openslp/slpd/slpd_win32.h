1682eae3768c9fda65d620c67319718425078d35
mpeterson <mpeterson@51fdc78b-28a9-475c-aff9-d797dd46749a>
2000-09-07 14:53:52 +0000

    Platform specific header files

---
+++ openslp/slpd/slpd_win32.h
@@ -0,0 +1,109 @@
+/***************************************************************************/
+/*                                                                         */
+/* Project:     OpenSLP - OpenSource implementation of Service Location    */
+/*              Protocol Version 2                                         */
+/*                                                                         */
+/* File:        slpd_win32.h                                               */
+/*                                                                         */
+/* Abstract:    Win32 specific header file                                 */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/* Copyright (c) 1995, 1999  Caldera Systems, Inc.                         */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU Lesser General Public License as published   */
+/* by the Free Software Foundation; either version 2.1 of the License, or  */
+/* (at your option) any later version.                                     */
+/*                                                                         */
+/*     This program is distributed in the hope that it will be useful,     */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of      */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       */
+/*     GNU Lesser General Public License for more details.                 */
+/*                                                                         */
+/*     You should have received a copy of the GNU Lesser General Public    */
+/*     License along with this program; see the file COPYING.  If not,     */
+/*     please obtain a copy from http://www.gnu.org/copyleft/lesser.html   */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/*     Please submit patches to http://www.openslp.org                     */
+/*                                                                         */
+/***************************************************************************/
+
+#include <windows.h>
+#include <winbase.h>
+#include <stdio.h>
+#include <winsock.h>
+
+/*  internal name of the service  */
+#define G_SERVICENAME        "slpd"    
+
+/*  displayed name of the service  */
+#define G_SERVICEDISPLAYNAME "Service Location Protocol"
+
+#if(!defined MAX_PATH)
+#define MAX_PATH    256
+#endif
+
+
+typedef DWORD               pid_t;
+typedef DWORD               uid_t;
+typedef int                 socklen_t;
+typedef DWORD               gid_t;
+typedef SOCKET              sockfd_t;
+
+/* enum detailing what to do with SLPD when launching it :
+   run in the console for debug, install it as a service or uninstall it */
+typedef enum _SLPDAction
+{
+  SLPD_DEBUG   = 0,
+  SLPD_INSTALL = 1,
+  SLPD_REMOVE  = 2
+} SLPDAction;
+
+
+/*=========================================================================*/
+VOID WINAPI SLPDServiceMain(DWORD argc, LPTSTR *argv); 
+/* Performs actual initialization of the service                           */
+/* This routine performs the service initialization and then calls the     */
+/* user defined ServiceStart() routine to perform majority of the work     */
+/*                                                                         */
+/* argc (IN)   number of command line arguments                            */
+/*                                                                         */
+/* argv (IN)  array of command line arguments                              */
+/*                                                                         */
+/*  returns  none                                                          */
+/*                                                                         */
+/*=========================================================================*/
+
+
+/*=========================================================================*/
+VOID SLPDCmdInstallService(); 
+/* Installs the SLPD service in the system                                 */
+/*                                                                         */
+/* returns  none                                                           */
+/*=========================================================================*/
+
+
+/*=========================================================================*/
+VOID SLPDCmdRemoveService(); 
+/* Removes the SLPD service from the system                                */
+/*                                                                         */
+/* returns  none                                                           */
+/*=========================================================================*/
+
+
+/*=========================================================================*/
+VOID SLPDCmdDebugService(int argc, char **argv);
+/* Runs SLPD in a terminal, to help debugging                              */
+/*                                                                         */
+/* argc (IN)   number of command line arguments                            */
+/*                                                                         */
+/* argv (IN)  array of command line arguments                              */
+/*                                                                         */
+/* returns  none                                                           */
+/*=========================================================================*/
+#endif /* SLPD_WIN32_H_INCLUDED */
+
+
