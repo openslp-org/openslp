b25f7be96f39b64b0914d528bb047b648c815d3c
hughes <hughes@51fdc78b-28a9-475c-aff9-d797dd46749a>
2000-09-27 21:35:23 +0000

    Added USE_PREDICATE define. And subdirectory stuff for libslpattr.

--- openslp/acconfig.h
+++ openslp/acconfig.h
@@ -1,7 +1,10 @@
 /* Local acconfig.h */
 
 /* defined if struct sigaction has member sa_restorer */
-#define HAVE_SA_RESTORER
+#undef HAVE_SA_RESTORER
 
 /* defined to size_t if <sys/socket.h> does not support socklen_t data type */
-#define socklen_t
+#undef socklen_t
+
+/* defined if predicates are enabled */
+#undef USE_PREDICATES
