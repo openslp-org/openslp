8efacd23253d7d9c482a159085ef0de33f03fe5c
mpeterson <mpeterson@51fdc78b-28a9-475c-aff9-d797dd46749a>
2001-04-13 15:47:49 +0000

    Definately needed to build ENABLE_AUTHENTICATION code

---
+++ openslp/slpd/slpd_spi.h
@@ -0,0 +1,76 @@
+/***************************************************************************/
+/*                                                                         */
+/* Project:     OpenSLP - OpenSource implementation of Service Location    */
+/*              Protocol Version 2                                         */
+/*                                                                         */
+/* File:        slpd_spi.h                                                 */
+/*                                                                         */
+/* Abstract:    Functions and variables for slpd usage of common SPI code  */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/*     Please submit patches to http://www.openslp.org                     */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/* Copyright (C) 2000 Caldera Systems, Inc                                 */
+/* All rights reserved.                                                    */
+/*                                                                         */
+/* Redistribution and use in source and binary forms, with or without      */
+/* modification, are permitted provided that the following conditions are  */
+/* met:                                                                    */ 
+/*                                                                         */
+/*      Redistributions of source code must retain the above copyright     */
+/*      notice, this list of conditions and the following disclaimer.      */
+/*                                                                         */
+/*      Redistributions in binary form must reproduce the above copyright  */
+/*      notice, this list of conditions and the following disclaimer in    */
+/*      the documentation and/or other materials provided with the         */
+/*      distribution.                                                      */
+/*                                                                         */
+/*      Neither the name of Caldera Systems nor the names of its           */
+/*      contributors may be used to endorse or promote products derived    */
+/*      from this software without specific prior written permission.      */
+/*                                                                         */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     */
+/* `AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      */
+/* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR   */
+/* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE CALDERA      */
+/* SYSTEMS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, */
+/* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        */
+/* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  */
+/* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON       */
+/* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT */
+/* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   */
+/* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    */
+/*                                                                         */
+/***************************************************************************/
+
+#ifndef SLPD_SPI_H_INCLUDED
+
+/*=========================================================================*/
+/* slpd includes                                                           */
+/*=========================================================================*/
+#include "slpd.h"
+
+/*=========================================================================*/
+/* Common code code includes                                               */
+/*=========================================================================*/
+#include "../common/slp_spi.h"  
+
+
+/*=========================================================================*/
+extern SLPSpiHandle G_SlpdSpiHandle;
+/*=========================================================================*/
+
+
+/*=========================================================================*/
+int SLPDSpiInit(const char* spifile);
+/*=========================================================================*/
+
+
+/*=========================================================================*/
+void SLPDSpiDeinit();
+/*=========================================================================*/
+
+#endif
