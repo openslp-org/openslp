cf6867e4f1217c293fac9f6be633603635199022
mpeterson <mpeterson@51fdc78b-28a9-475c-aff9-d797dd46749a>
2002-01-16 19:41:31 +0000

    Added setup.mk

---
+++ openslp.contrib/slptest/SETUP.mk
@@ -0,0 +1,14 @@
+#################################################
+# IMPORTANT:
+# The openslp sourcecode is required to compile
+# slptest.  
+#
+# Change the SLPROOT to the root of the openslp
+# source code
+#
+
+SLPROOT=<put directory here>
+
+TESTROOT=.
+
+VERSION="1.0.6"
