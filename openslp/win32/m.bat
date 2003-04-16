b1a089b93668dd97a61b02655bfc520ef7abead1
(no author) <(no author)@51fdc78b-28a9-475c-aff9-d797dd46749a>
2003-04-16 15:15:37 +0000

    This commit was manufactured by cvs2svn to create branch 'solers'.

--- openslp/win32/m.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd libslp
-call m.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-cd ..\slpd
-call m.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-cd ..\slptool
-call m.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-cd ..
-
--- openslp/win32/slptool/m.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist slptool.dep set NO_EXTERNAL_DEPS=1
-nmake /nologo /s /f slptool.mak CFG="slptool - Win32 Debug"  %1 %2 %3 %4 %5 %6 %7 %8 %9
-nmake /nologo /s /f slptool.mak CFG="slptool - Win32 Release"  %1 %2 %3 %4 %5 %6 %7 %8 %9
-set NO_EXTERNAL_DEPS=
