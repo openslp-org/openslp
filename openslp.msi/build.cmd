2dcb75a6fe6ac20c78e2d8c000d25dcd81568147
jcalcote <jcalcote@51fdc78b-28a9-475c-aff9-d797dd46749a>
2006-12-11 22:40:37 +0000

    Changed openslp windows msi installer over to Wix 3.0 tool set, and added vc8 runtime merge module to installer. Added missing win32 source files to unix distribution.

--- openslp.msi/build.cmd
+++ openslp.msi/build.cmd
@@ -1,53 +1,64 @@
 @echo off
 
-REM -------------------------------------------------------------------------
-REM Usage: build [version] [machine]
-REM 
-REM build.cmd rebuilds the openslp installer
-REM -------------------------------------------------------------------------
+rem -------------------------------------------------------------------------
+rem Usage: build [--ver <version>] [--arch <arch>] [--help]
+rem
+rem build.cmd rebuilds the openslp installer against wix 3.0 (build 1821)
+rem -------------------------------------------------------------------------
 
 setlocal
 
-if "%WIX_PATH"=="" goto usage
+if "%WIX_PATH"=="" goto dohelp
 
-REM Please ensure that your path contains the WIX tool set directory, and 
-REM that your environment contains a WIX_PATH variable that points to the
-REM root of the WIX tool set directory.
+rem -------------------------------------------------------------------------
+rem Please ensure that your path contains the WIX tool set directory, and 
+rem that your environment contains a WIX_PATH variable that points to the
+rem root of the WIX tool set directory.
 
 set version=2.0.0
 set machine=x86
 
-if "%1"=="" goto parm_done
-version=%1
+:doparse
+rem -------------------------------------------------------------------------
 shift
+if "%0%" == "--ver"     goto doversion
+if "%0%" == "--arch"    goto domachine
+if "%0%" == "--help"    goto dohelp
+if "%0%" == ""          goto dobuild
+goto dohelp
 
-if "%1"=="" goto parm_done
-machine=%1
+:doversion
 shift
+set version=%0%
+goto doparse
 
-:parm_done
+:domachine
+shift
+set machine=%0%
+goto doparse
 
+:dohelp
 echo.
-echo Building openslp_%version%_%machine%.msi...
-
-candle -nologo openslp.wxs openslp_files.wxs
-light -nologo -out openslp_%version%_%machine%.msi openslp.wixobj openslp_files.wixobj %WIX_PATH%\wixui.wixlib -loc %WIX_PATH%\WixUI_en-us.wxl
-
-goto done
-
-:usage
+echo Usage: build.cmd [--ver <version>] [--arch <arch>] [--help]
 echo.
-echo Usage: build.cmd [version] [machine]
+echo Where <version> is a three-part dot-separated version number of OpenSLP.
+echo and <arch> is a machine architecture tag, eg., x86, x86_64, alpha, etc.
+echo Use --help to get this help screen.
 echo.
-echo Where [version] is a three-part dot-separated version number of OpenSLP.
-echo and [machine] is a machine architecture tag, eg., x86, x86_64, alpha, etc.
-echo.
-echo The default configuration is version=2.0.0 and machine=x86.
+echo Default: "build --ver=%version% --arch=%machine%"
 echo.
 echo NOTE! Please ensure that the WIX_PATH environment variable contains the
 echo directory in which the WIX tool set has been installed.
+goto doexit
 
-:done
+:dobuild
+rem -------------------------------------------------------------------------
+echo.
+echo Building openslp_%version%_%machine%.msi...
 
+candle -nologo -dVERSION=%version% openslp.wxs
+light -nologo -out openslp_%version%_%machine%.msi openslp.wixobj -ext WixUIExtension -cultures:en-us -sval
+
+:doexit
 endlocal
 
--- openslp.msi/makesrc.cmd
+++ openslp.msi/makesrc.cmd
@@ -1,66 +1,78 @@
 @echo off
 
-REM -------------------------------------------------------------------------
-REM Usage: makesrc [<srcdir>] [<targetdir>] [{debug}|release]
-REM 
-REM makesrc.cmd creates either a debug or release code installation directory 
-REM structure from command line input.
-REM -------------------------------------------------------------------------
+rem -------------------------------------------------------------------------
+rem Usage: makesrc [--src <srcdir>] [--tgt <tgtdir>] [--debug] [--help]
+rem 
+rem makesrc.cmd creates either a debug or release code installation directory 
+rem structure from command line input.
+rem -------------------------------------------------------------------------
 
 setlocal
 
-REM check for all reasonable ways of asking for help
-if "%1"=="/help"  goto usage
-if "%1"=="-help"  goto usage
-if "%1"=="--help" goto usage
-if "%1"=="/h"     goto usage
-if "%1"=="-h"     goto usage
-if "%1"=="/?"     goto usage
-if "%1"=="-?"     goto usage
-if "%1"=="?"      goto usage
-
-REM gather parameters (if any)
 set source=..\openslp
 set target=.
+set build=release
+
+:doparse
+rem -------------------------------------------------------------------------
+shift
+if "%0%" == "--src"     goto dosource
+if "%0%" == "--tgt"     goto dotarget
+if "%0%" == "--debug"   goto dodebug
+if "%0%" == "--help"    goto dohelp
+if "%0%" == ""          goto dobuild
+goto dohelp
+
+:dosource
+shift
+set source=%0%
+goto doparse
+
+:dotarget
+shift
+set target=%0%
+goto doparse
+
+:dodebug
 set build=debug
+goto doparse
 
-if "%1"=="" goto parm_done
-set source=%1
-shift
+:dohelp
+echo.
+echo Usage: makesrc [--src <srcdir>] [--tgt <tgtdir>] [--debug] [--help]
+echo.
+echo Where <srcdir> is the location of the root of the openslp source directory, 
+echo and <tgtdir> is the location to build the install directory structure.
+echo The --debug option indicates that debug code should be packaged in the 
+echo resulting .msi file, rather than the default release code. Use --help to 
+echo get this help screen.
+echo.
+echo Default (release): "makesrc --src %source% --tgt %target%"
+echo.
+echo NOTE! Please ensure that the WIX_PATH environment variable contains the
+echo directory in which the WIX tool set has been installed.
+goto doexit
 
-if "%1"=="" goto parm_done
-set target=%1
-shift
+:dobuild
+rem -------------------------------------------------------------------------
+rem Check to be sure this is what they really want to do...
+echo Copying product [%build%] files from %source% into %target%\OpenSLP.
 
-if "%1"=="" goto parm_done
-set build=%1
-
-:parm_done
-
-REM check to be sure this is what they really want to do...
-echo Copying various [%build%] files from %source% into %target%\OpenSLP.
-echo Press Ctrl-C to quit now, otherwise
-pause
-
-REM remove old source directory (if exists), create new structure
+rem Remove old source directory (if exists), create new structure
 rmdir /s /q %target%\OpenSLP
 
+rem Create target directory structure
 mkdir %target%\OpenSLP
 mkdir %target%\OpenSLP\Docs
 mkdir %target%\OpenSLP\Docs\html
 mkdir %target%\OpenSLP\Include
 mkdir %target%\OpenSLP\Lib
 
-REM copy files from %source% to %target%[%build%]
+rem Copy files from %source% to %target%[%build%]
 xcopy %source%\doc\rfc\* %target%\OpenSLP\Docs\rfc /S /I
 xcopy %source%\doc\html\* %target%\OpenSLP\Docs\html /S /I
-xcopy %source%\doxygen\smalllogo.jpg %target%\OpenSLP\Docs\html
-xcopy %source%\doxygen\html\* %target%\OpenSLP\Docs\html\SourceCode /S/I
-
 xcopy %source%\libslp\slp.h %target%\OpenSLP\Include
-
 xcopy %source%\win32\%build%\libslp.lib %target%\OpenSLP\Lib
-
 xcopy %source%\win32\%build%\libslp.dll %target%\OpenSLP
 xcopy %source%\win32\%build%\libslp.pdb %target%\OpenSLP
 xcopy %source%\win32\%build%\slpd.exe %target%\OpenSLP
@@ -68,23 +80,6 @@ xcopy %source%\win32\%build%\slpd.pdb %target%\OpenSLP
 xcopy %source%\win32\%build%\slptool.exe %target%\OpenSLP
 xcopy %source%\win32\%build%\slptool.pdb %target%\OpenSLP
 
-goto done
-
-:usage
-echo.
-echo Usage: makesrc [<srcdir>] [<targetdir>] [debug|release]
-echo.
-echo Where <srcdir> is the location of the root of the openslp source directory, 
-echo and <targetdir> is the location to build the install directory structure,
-echo and debug|release represents the build from which to pull win32 binaries
-echo when building the installation image.
-echo.
-echo The default command line is: "makesrc ..\openslp . debug"
-echo.
-echo NOTE! Please ensure that the WIX_PATH environment variable contains the
-echo directory in which the WIX tool set has been installed.
-
-:done
-
+:doexit
 endlocal
 
