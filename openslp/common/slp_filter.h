7d587d57cdba5e6c0eaf3818ac029c6797980f5f
jcalcote <jcalcote@51fdc78b-28a9-475c-aff9-d797dd46749a>
2006-12-15 23:52:55 +0000

    Incorporated Mike Day's copyright assignment to openslp's bsd style license, and bug fixes in attr/filter lexers and parsers. Changed Caldera's internal GPL licensed files in contrib section to Caldera's BSD style license. All code is now BSD style license only.

--- openslp/common/slp_filter.h
+++ openslp/common/slp_filter.h
@@ -28,24 +28,6 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Originated: 04-21-2001 
- * Copyright (C) Michael Day, 2001 
- *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation; either version 2 
- * of the License, or (at your option) any later version. 
- *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License for more details. 
- *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
- * 02111-1307, USA. 
  *-------------------------------------------------------------------------*/
 
 /** Header file for encoding and decoding LDAP filters.
