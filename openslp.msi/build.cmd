cdd0d64770084a016ded39a1279f294678f436bd
jcalcote <jcalcote@51fdc78b-28a9-475c-aff9-d797dd46749a>
2011-06-11 04:24:28 +0000

    Update WIX project files for building msi installer; changes for WIX 3.5.

--- openslp.msi/build.cmd
+++ openslp.msi/build.cmd
@@ -17,7 +17,7 @@ rem root of the WIX tool set directory.
 
 set package=openslp
 set version=2.0.0
-set revision=beta1
+set revision=beta2
 set machine=x86
 
 :doparse
@@ -58,8 +58,8 @@ rem -------------------------------------------------------------------------
 echo.
 echo Building openslp_%version%_%revision%_%machine%.msi...
 
-%WIX_PATH%\bin\candle -nologo -dVERSION=%version% openslp.wxs
-%WIX_PATH%\bin\light -nologo -out %package%_%version%_%revision%_%machine%.msi openslp.wixobj -ext WixUIExtension -cultures:en-us -sval
+%WIX_PATH%\candle -nologo -dVERSION=%version% -dREVISION=%revision% openslp.wxs
+%WIX_PATH%\light -nologo -out %package%_%version%_%revision%_%machine%.msi openslp.wixobj -ext WixUIExtension -cultures:en-us -sval
 
 :doexit
 endlocal
